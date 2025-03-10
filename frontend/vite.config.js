176f00ad468da9ddf8ff1f2a14af8dec45ffa6ed
omar semgey <110037975+omarSemgey@users.noreply.github.com>
2025-03-10 22:22:34 +0300

    Migrate to Cloudflare Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
     react(),
     visualizer({ open: false })
   ],
+  base: '/',
   build: {
+    outDir: 'dist',
     minify: 'terser',
     sourcemap: false,
     cssCodeSplit: true,
     rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        '404': path.resolve(__dirname, 'public/404.html')
+      },
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
